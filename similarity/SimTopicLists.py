d976f8f2da89b87a732b6e3a44bd5e50f2c6b8a4
Renata1995 <zchai13@austincollege.edu>
2016-07-25 09:38:20 -0500

    modified SimtopicLists.py

--- similarity/SimTopicLists.py
+++ similarity/SimTopicLists.py
@@ -258,6 +258,36 @@ class SimTopicLists:
             file.write("</tr>")
         file.write("</tbody></table>")
 
+    def show_results_values(self, distance_list, file):
+        """
+        Show the results from the comp_topic_lists method
+        :param distance_list: a 2D list of data
+        """
+        width = 14
+        file.write("\ntopic List 1 is vertical and topic List 2 is horizontal\n")
+
+        # calculate smallest results
+        # show row labels
+        file.write("<style>table, th, td {border: 1px solid black;}</style>")
+        file.write("<table><thead><tr><th></th>")
+        for value in range(len(distance_list[0])):
+            file.write("<th> topic" + str(value) + "</th>")
+        file.write("</tr></thead><tbody>")
+
+        for index, sublist in enumerate(distance_list):
+            # show column labels
+            file.write("<tr><td> topic" + str(index) + "</td>")
+            # rank
+            for sub_i, value in enumerate(sublist):
+                colornum = str(int(255 * float(value)))
+                rgbstr = "rgb(" + colornum + "," + colornum + "," + colornum + ")"
+                file.write("<td><span style='background-color: " + rgbstr + "'>")
+                file.write(value.split()[1])
+                file.write("</span></td>")
+            file.write("</tr>")
+        file.write("</tbody></table>")
+
+
     def show_results(self, distance_list, file):
         """
         Show the results from the comp_topic_lists method
