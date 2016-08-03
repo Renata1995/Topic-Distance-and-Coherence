9008f71c143b38f67840ce76a24d1694d5fd4c15
unknown <CHAI@CSGRAD15.ecs.baylor.edu>
2016-08-02 23:56:34 -0500

    coherence plot

--- log_plot_3comp.py
+++ log_plot_3comp.py
@@ -9,11 +9,11 @@ else:
     topics_count = sys.argv[1]
 
 start_wc = 10
-stop_wc = 260
-step_wc = 10
+stop_wc = 150
+step_wc = 5
 # get tc values based on randomly generated numbers
 randlist = []
-for num in range(start_wc, stop_wc, step_wc):
+for num in range(10,150,10):
     ifile = open("reuters_LDA/tc_rand_" + str(num) + ".txt")
     avg = ifile.readline().split()[1]
     sd = ifile.readline().split()[1]
@@ -24,10 +24,10 @@ rand_avglist = [value[0] for value in randlist]
 # get tc values from topics
 tlist = []
 for num in range(start_wc, stop_wc, step_wc):
-    ifile = open("LDA_pp_reuters_tfidf_t" + str(topics_count) + "/top_topics_" + str(num) + ".txt")
+    ifile = open("LDA_pp_reuters_tfidf_t" + str(topics_count) + "/top_topics_" + str(num) + "_start0.txt")
     sub_tclist = []
     for line in ifile:
-        if "Topic" in line:
+        if "topic" in line:
             # format in the ifile: Topic num tc_value
             sub_tclist.append(float(line.split()[2]))
     # tuple: (most positive, most negative, avg, sd)
@@ -35,10 +35,10 @@ for num in range(start_wc, stop_wc, step_wc):
 
 blist = []
 for num in range(start_wc, stop_wc, step_wc):
-    ifile = open("LDA_pp_reuters_binary_t" + str(topics_count) + "/top_topics_" + str(num) + ".txt")
+    ifile = open("LDA_pp_reuters_binary_t" + str(topics_count) + "/top_topics_" + str(num) + "_start0.txt")
     sub_tclist = []
     for line in ifile:
-        if "Topic" in line:
+        if "topic" in line:
             # format in the ifile: Topic num tc_value
             sub_tclist.append(float(line.split()[2]))
     # tuple: (most positive, most negative, avg, sd)
@@ -46,18 +46,19 @@ for num in range(start_wc, stop_wc, step_wc):
 
 clist = []
 for num in range(start_wc, stop_wc, step_wc):
-    ifile = open("LDA_pp_reuters_bow_t" + str(topics_count) + "/top_topics_" + str(num) + ".txt")
+    ifile = open("LDA_pp_reuters_bow_t" + str(topics_count) + "/top_topics_" + str(num) + "_start0.txt")
     sub_tclist = []
     for line in ifile:
-        if "Topic" in line:
+        if "topic" in line:
             # format in the ifile: Topic num tc_value
             sub_tclist.append(float(line.split()[2]))
     # tuple: (most positive, most negative, avg, sd)
     clist.append(np.average(sub_tclist))
+    
 
 # plot
 fig = plt.figure()
-fig.suptitle("UMass Topic Coherence \nReuters 3 Corpus Type Comparison(3 topics)", fontsize=15)
+fig.suptitle("Co-occurence Based Topic Coherence \nReuters 3 Corpus Type Comparison(3 topics)", fontsize=15)
 
 plt.ylabel("log( - topic coherence)")
 plt.xlabel("log(# of words)")
@@ -65,7 +66,10 @@ plt.xlabel("log(# of words)")
 # red dashes, blue squares and green triangles
 x_axis = range(start_wc, stop_wc, step_wc)
 x_axis = [np.log(v) for v in x_axis]
-line_rand, = plt.plot(x_axis, [np.log(-float(v)) for v in rand_avglist], color=(0, 0, 0), marker="o")
+
+rand_x = list(range(10,150,10))
+rand_x = [np.log(v) for v in rand_x]
+line_rand, = plt.plot(rand_x, [np.log(-float(v)) for v in rand_avglist], color=(0, 0, 0), marker="o")
 for x, y in zip(x_axis, rand_avglist):
     plt.annotate("{:.2f}".format(float(y)), xy=(x, y))
 
@@ -73,8 +77,10 @@ line_pos, = plt.plot(x_axis, [np.log(-v) for v in tlist], "g", marker="^")
 
 line_neg, = plt.plot(x_axis, [np.log(-v) for v in clist], "b", marker="^")
 
-line_avg, = plt.plot(x_axis, [np.log(-v) for v in blist], "y", marker="^")
+line_avg, = plt.plot(x_axis, [np.log(-v) for v in blist], "r", marker="^")
 
 plt.legend([line_rand, line_pos, line_neg, line_avg], ["random words", "tfidf", "bow", "binary"], loc="lower right")
 
-plt.show()
+fig = plt.gcf()
+fig.set_size_inches(16, 12)
+plt.savefig("plot_3comp_tc_t" + str(topics_count) + "_reuters" + ".png")
